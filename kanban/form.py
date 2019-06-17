8db34245dcd00b4952e2940b364934fc22927f76
kazu-tsukagoshi <kazu.tsukagoshi539@gmail.com>
2019-06-17 11:15:10 +0900

    make it better

---
+++ kanban/form.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import List, Card
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ("username", "last_name", "first_name", "email",)
+
+class ListForm(forms.ModelForm):
+
+    class Meta:
+        model = List
+        fields = ("title",)
+
+class CardForm(forms.ModelForm):
+
+    class Meta:
+        model = Card
+        fields = ("title","description","list")
+
+class CardCreateFromHomeForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Card
+        fields = ("title", "description",)
